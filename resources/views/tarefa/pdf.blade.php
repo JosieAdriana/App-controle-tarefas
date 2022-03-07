3adc2c0e6d29a47433e27fb686c0b846ad118ddd
Josie Flôres <drijosie@gmail.com>
2022-03-07 10:09:13 -0300

    Adicionando suporte UTF-8, estilos CSS e quebras de páginas nas Views de PDF

--- resources/views/tarefa/pdf.blade.php
+++ resources/views/tarefa/pdf.blade.php
@@ -1,23 +1,66 @@
-<h2>Lista de tarefas</h2>
+<html>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Tarefa</th>
-            <th>Data Limite Conclusão</th>
-        </tr>
-    </thead>
+<head>
 
-    <tbody>
-        @foreach ($tarefas as $key => $tarefa)
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <style>
+        .page-break {
+            page-break-after: always;
+        }
+
+        .titulo {
+            border: 1px;
+            background-color: #c2c2c2;
+            text-align: center;
+            width: 100%;
+            text-transform: uppercase;
+            font-weight: bold;
+            margin-bottom: 25px;
+        }
+
+        table th {
+            text-align: left
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+    <div class="titulo">Lista de tarefas</div>
+
+    <table style="width: 100%">
+        <thead>
             <tr>
-                <td>{{ $tarefa->id }}</td>
-                <td>{{ $tarefa->tarefa }}</td>
-                <td>{{ date('d/m/y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+                <th>ID</th>
+                <th>Tarefa</th>
+                <th>Data Limite Conclusão</th>
             </tr>
-        @endforeach
+        </thead>
 
-    </tbody>
+        <tbody>
+            @foreach ($tarefas as $key => $tarefa)
+                <tr>
+                    <td>{{ $tarefa->id }}</td>
+                    <td>{{ $tarefa->tarefa }}</td>
+                    <td>{{ date('d/m/y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+                </tr>
+            @endforeach
 
-</table>
+        </tbody>
+
+    </table>
+
+    <div class="page-break"></div>
+    <h2>Página 2</h2>
+
+    <div class="page-break"></div>
+    <h2>Página 3</h2>
+
+    <div class="page-break"></div>
+    <h2>Página 4</h2>
+</body>
+
+</html>
